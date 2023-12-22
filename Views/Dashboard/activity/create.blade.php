6d3a362fb84290c31d31f5690ef069bb7709b9fc
IKRAAM ZULFADZLI BIN ABDULLAH <38940680+ikraam94@users.noreply.github.com>
2023-12-22 19:18:01 +0800

    Create create.blade.php

---
+++ Views/Dashboard/activity/create.blade.php
@@ -0,0 +1,23 @@
+@extends('dashboard.layout')
+
+@section('content')
+<div class="pb-3"><a href="{{ route('activity.index') }}" class="btn btn-secondary"> Back </a></div>
+<form action="{{ route('activity.store') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+          <label for="activity" class="form-label">Activity</label>
+          <input type="text" class="form-control form-control-sm" name="activity" id="Activity" aria-describedby="helpId" placeholder="Activity" value="{{ Session::get('activity') }}">
+        </div>
+        <div class="mb-3">
+          <div class="row">
+            <div class="col-auto">Starting Date</div>
+            <div class="col-auto"><input type="date" class="form-control form-control-sm" name="starting_date" placeholder="dd/mm/yyy" value="{{ Session::get('starting_date') }}"></div>
+            <div class="col-auto">Ending Date</div>
+            <div class="col-auto"><input type="date" class="form-control form-control-sm" name="ending_date" placeholder="dd/mm/yyy" value="{{ Session::get('ending_date') }}"></div>
+        <div class="mb-3">
+            <label for="content" class="form-label">Content</label>
+            <textarea class="form-control summernote" rows="5" name="content">{{ Session::get('content') }}</textarea>
+          </div>
+          <button class="btn btn-primary" name="save" type="submit">SAVE</button>
+    </form>
+@endsection
