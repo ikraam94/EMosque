a0cec7ac3645005bffe31bf221b52728154e3fb4
IKRAAM ZULFADZLI BIN ABDULLAH <38940680+ikraam94@users.noreply.github.com>
2023-12-22 18:28:16 +0800

    Add files via upload

---
+++ App/Models/homepage.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class homepage extends Model
+{
+    use HasFactory;
+    protected $table = "homepage";
+    protected $fillable = ['title','content'];
+ 
+}
